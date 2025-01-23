d569c68e13595a5baea7c77c544e32d38a748bbb
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-01-23 17:55:42 -0300

    created participante entity, mapped orm and repository layer

--- src/main/java/com/example/demo/domain/models/entities/Evento.java
+++ src/main/java/com/example/demo/domain/models/entities/Evento.java
@@ -1,11 +1,17 @@
 package com.example.demo.domain.models.entities;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
+
+import com.example.demo.domain.models.enums.Status;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -23,9 +29,19 @@ public class Evento {
 	
 	@Column(name = "data_inicio", columnDefinition = "TIMESTAMP")
 	private LocalDateTime dataInicio;
+	
 	@Column(name = "data_fim", columnDefinition = "TIMESTAMP")
 	private LocalDateTime dataFim;
+	
+	@Column(length = 100)
 	private String local;
-	private Integer limite_participantes;
-	// TODO private Status status;
+	
+	@Column(name = "limite_participantes")
+	private Integer limiteParticipantes;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
+	
+	@OneToMany(mappedBy = "evento")
+	private Set<Participante> participantes;
 }
---
+++ src/main/java/com/example/demo/domain/models/entities/Participante.java
@@ -0,0 +1,33 @@
+package com.example.demo.domain.models.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "participantes")
+@Data
+public class Participante {
+
+	@Id
+	private UUID id;
+	
+	@Column(length = 50, nullable = false)
+	private String nome;
+	
+	@Column(length = 15)
+	private String telefone;
+	
+	@Column(length = 50, nullable = false)
+	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "evento_id")
+	private Evento evento;
+}
